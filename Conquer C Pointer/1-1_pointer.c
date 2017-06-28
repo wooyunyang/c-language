e73d0256563d7e75cebfc151646e6672a8456a2a
Daniel Zhang <danielzhme@gmail.com>
2017-06-29 00:59:03 +0800

    添加Conquer C Pointer/1-1_pointer.c。

---
+++ Conquer C Pointer/1-1_pointer.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int hoge = 5;
+	int piyo = 10;
+	int *hoge_p;
+	
+	/* 输出每个变量的值 */
+	printf("&hoge.. %p\n", &hoge);
+	printf("&hiyo.. %p\n", &piyo);
+	printf("&hoge_p.. %p\n", &hoge_p);
+	
+	/* 将hoge的地址赋予hoge_p */
+	hoge_p = &hoge;
+	printf("hoge_p.. %p\n", hoge_p);
+	
+	/* 通过hoge_p输出hoge的内容 */
+	printf("*hoge_p.. %d\n", *hoge_p);
+	
+	/* 通过hoge_p修改hoge的内容 */
+	*hoge_p = 10;
+	printf("hoge.. %d\n", hoge);
+	
+	return 0;
+}
